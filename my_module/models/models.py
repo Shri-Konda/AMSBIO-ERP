fc689f5391949f676da4dc15930e0e9d3ad6604a
vaibhavnathj-amsbio <vaibhavnathj@amsbio.com>
2021-08-26 16:21:30 +0100

    added mdl description

--- my_module/models/models.py
+++ my_module/models/models.py
@@ -5,6 +5,7 @@ from odoo import models, fields, api
 
 class my_module(models.Model):
     _name = 'my_module.my_module'
+    _description = "This is a sample module, which will be used for holding custom templates in future."
 
     name = fields.Char()
     value = fields.Integer()
