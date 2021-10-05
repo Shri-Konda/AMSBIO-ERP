960a61852128163b127d1c27184fc56cc9510e15
vaibhavnathj-amsbio <vaibhavnathj@amsbio.com>
2021-10-05 16:04:29 +0200

    added all tmplts

--- my_module/__manifest__.py
+++ my_module/__manifest__.py
@@ -13,13 +13,18 @@
     'website': "",
 
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
+        'views/DeliveryNotesTemplate.xml',
+        'views/InvoiceTemplate.xml',
         'views/OrderQuotationTemplate.xml',
+        'views/PurchaseOrderTemplate.xml',
+        'views/RemittanceTemplate.xml',
+        'views/RFQTemplate.xml',
     ],
 }
