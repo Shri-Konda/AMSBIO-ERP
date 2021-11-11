e9316b657553b21830852770ce72c1be4f339d1f
Shri-Konda <83066870+Shri-Konda@users.noreply.github.com>
2021-11-11 02:34:11 +0000

    Automaion and other changes
    •	Automation of Customer Invoices in Subsidiary Companies.
    •	Customer reference will be carried forward from Subsidiary sales order to UK sales order, delivery order, Picking operation report and Delivery slip.
    •	Customer reference will be unique for every customer.
    •	Delivery time (weeks) field will be mandatory and Blank by default.
    •	Automatic confirmation of Purchase order in Subsidiary Company.

--- ti_warehouse_count/models/inherit_sale_order_line.py
+++ ti_warehouse_count/models/inherit_sale_order_line.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+# https://support.targetintegration.com/issues/6728
 
+from odoo.exceptions import UserError
 from odoo import api, fields, models, _
 from logging import getLogger
 _logger = getLogger(__name__)
+
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+
+    is_vendor_updated = fields.Boolean('Is vendor updated',default=False)
+    is_reordering_updated = fields.Boolean('Is reordering updated',default=False)
+
 
 class Product(models.Model):
     _inherit = "product.product"
@@ -18,7 +30,7 @@ class Product(models.Model):
         quants_res = Quant.search(domain_quant).mapped('available_quantity',)
 
         if quants_res:
-            return quants_res[0]
+            return sum(quants_res)
         return 0
 
 
@@ -29,23 +41,33 @@ class SaleOrderLine(models.Model):
 
     uk_warehouse_qty = fields.Float('Available Qty For UK Warehouse', readonly=True, compute='_compute_warehouse_qty')
     uk_us_warehouse_qty = fields.Float('Available Qty For UK-US Warehouse', readonly=True, compute='_compute_warehouse_qty')
+    ch_warehouse_qty = fields.Float('Available Qty For CH Warehouse', readonly=True, compute='_compute_warehouse_qty')
+    bv_warehouse_qty = fields.Float('Available Qty For BV Warehouse', readonly=True, compute='_compute_warehouse_qty')
 
 
     @api.depends('product_id')
     def _compute_warehouse_qty(self):
         for line in self:
-            if line.company_id.name =='AMS Biotechnology (Europe) Limited':
+            IrDefault = self.env['ir.default'].sudo()
+            company_id = IrDefault.get('res.config.settings', 'warehouse_count')
+            company_id = self.env['res.company'].sudo().browse([company_id])
+            if company_id and line.company_id.id == company_id.id:
                 warehouses = self.env['stock.warehouse'].sudo().search([("company_id","=",line.company_id.id)])
-                
-                if len(warehouses)==2:
+                if len(warehouses)==4:
                     line.uk_warehouse_qty = line.product_id._get_available_qty(warehouse=warehouses[0])
                     line.uk_us_warehouse_qty = line.product_id._get_available_qty(warehouse=warehouses[1])
+                    line.ch_warehouse_qty = line.product_id._get_available_qty(warehouse=warehouses[2])
+                    line.bv_warehouse_qty = line.product_id._get_available_qty(warehouse=warehouses[3])
                 else:
                     line.uk_warehouse_qty = 0
                     line.uk_us_warehouse_qty = 0
+                    line.ch_warehouse_qty = 0
+                    line.bv_warehouse_qty = 0
             else:
                     line.uk_warehouse_qty = 0
                     line.uk_us_warehouse_qty = 0
+                    line.ch_warehouse_qty = 0
+                    line.bv_warehouse_qty = 0
 
 
 
@@ -54,20 +76,58 @@ class PurchaseOrderLine(models.Model):
 
     uk_warehouse_qty = fields.Float('Available Qty For UK Warehouse', readonly=True, compute='_compute_warehouse_qty')
     uk_us_warehouse_qty = fields.Float('Available Qty For UK-US Warehouse', readonly=True, compute='_compute_warehouse_qty')
+    ch_warehouse_qty = fields.Float('Available Qty For CH Warehouse', readonly=True, compute='_compute_warehouse_qty')
+    bv_warehouse_qty = fields.Float('Available Qty For BV Warehouse', readonly=True, compute='_compute_warehouse_qty')
 
 
     @api.depends('product_id')
     def _compute_warehouse_qty(self):
         for line in self:
-            if line.company_id.name=='AMS Biotechnology (Europe) Limited':
+            IrDefault = self.env['ir.default'].sudo()
+            company_id = IrDefault.get('res.config.settings', 'warehouse_count')
+            company_id = self.env['res.company'].sudo().browse([company_id])
+            if company_id and line.company_id.id == company_id.id:
                 warehouses = self.env['stock.warehouse'].sudo().search([("company_id","=",line.company_id.id)])
-                if len(warehouses)==2:
+                if len(warehouses)==4:
                     line.uk_warehouse_qty = line.product_id._get_available_qty(warehouse=warehouses[0])
                     line.uk_us_warehouse_qty = line.product_id._get_available_qty(warehouse=warehouses[1])
+                    line.ch_warehouse_qty = line.product_id._get_available_qty(warehouse=warehouses[2])
+                    line.bv_warehouse_qty = line.product_id._get_available_qty(warehouse=warehouses[3])
                 else:
                     line.uk_warehouse_qty = 0
                     line.uk_us_warehouse_qty = 0
+                    line.ch_warehouse_qty = 0
+                    line.bv_warehouse_qty = 0
             else:
                     line.uk_warehouse_qty = 0
                     line.uk_us_warehouse_qty = 0
+                    line.ch_warehouse_qty = 0
+                    line.bv_warehouse_qty = 0
 
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    _sql_constraints = [('customer_ref_partner_uniq', 'unique (client_order_ref,partner_id)', _('The Customer Reference must be unique per Customer!'))]
+
+
+    @api.onchange('delivery_time_week')
+    def check_field_type(self):
+        for rec in self:
+            if rec.delivery_time_week and rec.delivery_time_week.strip()!="":
+                try:
+                    rec.delivery_time_week = int(rec.delivery_time_week.strip())
+                except Exception as e:
+                    raise UserError(_(f'Invalid Field Value.'))
+
+
+    delivery_time_week = fields.Char('Delivery Time (Weeks)')
+
+
+    def _action_confirm(self):
+        result = super(SaleOrder, self)._action_confirm()
+        for order in self:
+            po = order._get_purchase_orders()
+            if po and len(po)==1:
+                po.button_confirm()
+        return result
