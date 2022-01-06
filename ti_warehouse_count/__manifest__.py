fb2a685dc7e5230ddb6bac160e09029fb2b77197
amit <amit.kumar@targetintegration.com>
2022-01-06 18:18:22 +0530

    [UPD] ti_warehouse_count updated module with currency rate support

--- ti_warehouse_count/__manifest__.py
+++ ti_warehouse_count/__manifest__.py
@@ -2,7 +2,7 @@
   "name"                 :  "TI Warehouse count",
   "summary"              :  """TI-Warehouse Count.""",
   "category"             :  "Inventory/Inventory",
-  "version"              :  "1.0.17",
+  "version"              :  "1.0.18",
   "sequence"             :  1,
   "author"               :  "Target Integration.",
   "website"              :  "http://www.targetintegration.com",
