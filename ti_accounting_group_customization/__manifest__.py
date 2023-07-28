153839ea14162ca7e9dcd20d0a3a3e6eb7264379
karan.bk <karan.bk@targetintegration.com>
2023-07-28 22:01:56 +0530

    [UPDATE] ti_accounting_group_customization: display readonly currencies for user group 2

--- ti_accounting_group_customization/__manifest__.py
+++ ti_accounting_group_customization/__manifest__.py
@@ -8,10 +8,11 @@
   "website"              :  "http://www.targetintegration.com",
   "description"          :  """Accounting Group Customization""",
   "depends"              :  [
-                             'account',
+                             'account_accountant',
                             ],
   "data"                 :  [
                               'security/ir_group.xml',
+                              'views/group_user_views.xml'
                             ],
   "application"          :  True,
   "installable"          :  True,
