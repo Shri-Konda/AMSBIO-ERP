5b4b5078b752e5bbcc23b3f7f127fa0366f46784
KaranB2010 <karan.bk@targetintegration.com>
2023-11-15 16:03:26 +0530

    [UPD] ti_amsbio_edi: use Path to create csv files

--- ti_amsbio_edi/models/sale_order.py
+++ ti_amsbio_edi/models/sale_order.py
@@ -3,6 +3,7 @@
 import os
 import csv
 import logging
+from pathlib import Path
 from psycopg2 import IntegrityError, InternalError
 from odoo.tools import format_date
 from odoo import models, fields, api, _, Command
@@ -321,7 +322,7 @@ class SaleOrder(models.Model):
         order_export_folder = self.ftp_server_id.get_export_orders_folder()
         order_values = self._prepare_order_confirmation_csv_values()
         if order_values:
-            filepath = f"{order_export_folder}/ACK_{self.name.replace('/', '_')}.csv"
+            filepath = Path(f"{order_export_folder}/ACK_{self.name.replace('/', '_')}.csv")
             with open(filepath, "w") as csv_file:
                 csv_writer = csv.writer(csv_file)
                 csv_writer.writerows(order_values)
