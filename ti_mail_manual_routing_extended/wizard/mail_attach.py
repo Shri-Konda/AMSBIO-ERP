3ab6bb1d28a93475e71b33e8a6d93fad1d61b566
karan <karan.bk@targetintegration.com>
2023-12-20 22:31:58 +0530

    [UPD] ti_mail_manual_routing: updated for tab visibility

--- ti_mail_manual_routing_extended/wizard/mail_attach.py
+++ ti_mail_manual_routing_extended/wizard/mail_attach.py
@@ -12,10 +12,10 @@ class MailAttachWizard(models.TransientModel):
         "override method to update previous converstation field on lead/contact/sale order"
 
         result = super(MailAttachWizard, self).action_attach_mail_message()
-        _logger.info(f"\n==>res_reference: {self.res_reference._name}")
+        # _logger.info(f"\n==>res_reference: {self.res_reference._name}")
         if self.res_reference._name in ["crm.lead", "res.partner", "sale.order"]:
             record = self.env[self.res_reference._name].browse(self.res_reference.id)
-            _logger.info(f"\n==>record: {record}")
+            # _logger.info(f"\n==>record: {record}")
             record.write({'amsbio_previous_conversation': "\n".join(self.message_ids.mapped('body'))})
 
         return result
