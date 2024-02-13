7b213d7112154de07c4e3aa5e698ccf6bf888183
KaranB2010 <karan.bk@targetintegration.com>
2024-02-13 23:16:52 +0530

    [UPD] ti_amsbio_edi: add street3 on the pdf reports

--- ti_amsbio_edi/models/res_partner.py
+++ ti_amsbio_edi/models/res_partner.py
@@ -1,6 +1,9 @@
 # -*-coding: utf-8 -*-
 
-from odoo import models, fields
+import logging
+from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
 
 
 class Partner(models.Model):
@@ -8,3 +11,22 @@ class Partner(models.Model):
 
     amsbio_edi_account_number = fields.Char("EDI Account Number", readonly=True, copy=False, help="Unique account number for the customer whose orders are created through EDI integration.")
     street3 = fields.Char(string="Street 3")
+
+    @api.model
+    def _formatting_address_fields(self):
+        "override method to add street3 on the format"
+
+        address_format = super(Partner, self)._formatting_address_fields()
+        if address_format:
+            address_format.append("street3")
+
+        return address_format
+    
+    @api.model
+    def _get_address_format(self):
+        "override mthod to add street3 on the format"
+        
+        address_format = super(Partner, self)._get_address_format()
+        if address_format:
+            return address_format.replace("%(street2)s", "%(street2)s\n%(street3)s")
+        return address_format
