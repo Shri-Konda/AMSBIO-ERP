689edf8730dc702f17f38360d24fe3cf5fabb359
amit.kumar <amit.kumar@targetintegration.com>
2024-02-13 18:34:55 +0530

    [UPD] update in event custom module

--- ti_website_crm_event_amsbio/static/src/js/intrest.js
+++ ti_website_crm_event_amsbio/static/src/js/intrest.js
@@ -44,9 +44,21 @@ odoo.define('ti_website_crm_event_amsbio.onchange_intrest', function (require) {
 
 
         $("input.sub_intrest_tag").change(function() {
+            var parent_tag_id = $(this).attr("parent_tag_id")
+            console.log($("[parent_tag_id=" + parent_tag_id + "]:checked"));
+
             if ($(this).prop('checked') == true){
                 $(".sub_intrest_warning_msg").hide();
             }
+
+            // Start: Code to add color to parent tag
+            if($("[parent_tag_id=" + parent_tag_id + "]:checked").length > 0){
+                $("label[for=tag_id_" + parent_tag_id + "]").addClass("text-success")
+            }
+            else{
+                $("label[for=tag_id_" + parent_tag_id + "]").removeClass("text-success")
+            }
+            //End
         });
 
         $("#ti_new_lead_form").submit(function(ev){
